0bf3f94e8d532de3754c5f480b1760dd8acca65e
Sebastian Firsaev <91489950+Sebastian-Firsaev@users.noreply.github.com>
2023-11-23 22:33:47 +0000

    Update rendezvous.cpp

--- lab2/rendezvous.cpp
+++ lab2/rendezvous.cpp
@@ -1,46 +1,67 @@
+/**
+ * @file main.cpp
+ * @brief Usage of Semaphores for thread synchronization
+ * @version 1.0
+ * @license This lab is licensed under the GPL-3
+ * @author Sebastian Firsaev
+ */
+
 #include "Semaphore.h"
 #include <thread>
 #include <vector>
 #include <iostream>
+#include <atomic>
 
+std::atomic<int> counter(0);
 
-/*Sebastian Firsaev */
-
-/*! displays the first function in the barrier being executed */
+/**
+ * @brief Task function executed by each thread
+ * @param mutexSem A shared pointer to Semaphore object for mutual exclusion
+ * @param barrierSem A shared pointer to Semaphore object for synchronization
+ * @param threadCount Number of threads
+ */
 void task(std::shared_ptr<Semaphore> mutexSem, std::shared_ptr<Semaphore> barrierSem, int threadCount) {
-  for (int i = 0; i < 5; i++) {
+  for (int i = 0; i < threadCount; i++) {
     std::cout << "first" << std::endl;
 
-    // Wait for all threads to reach this point (barrier)
-    barrierSem->wait();
+    // Increment the counter and wait until all threads have incremented it
+    counter++;
+    if (counter.load() == threadCount) {
+      barrierSem->Signal(); // Release the barrier
+    } else {
+      barrierSem->Wait(); // Wait for others to increment
+    }
 
     std::cout << "second" << std::endl;
 
-    // Wait for all threads to finish the "second" part before looping
-    barrierSem->wait();
+    // Increment the counter and wait until all threads have incremented it
+    counter++;
+    if (counter.load() == 2 * threadCount) {
+      barrierSem->Signal(); // Release the barrier
+    } else {
+      barrierSem->Wait(); // Wait for others to increment
+    }
   }
 }
 
-
-
-
-int main(void){
-  std::shared_ptr<Semaphore> mutexSem;
-  std::shared_ptr<Semaphore> barrierSem;
+/**
+ * @brief Main function
+ * @return Program exit status
+ */
+int main(void) {
+  std::shared_ptr<Semaphore> mutexSem = std::make_shared<Semaphore>(1);
+  std::shared_ptr<Semaphore> barrierSem = std::make_shared<Semaphore>(0);
   int threadCount = 5;
-  mutexSem=std::make_shared<Semaphore>(1);
-  barrierSem=std::make_shared<Semaphore>(0);
-  /*!< An array of threads*/
-  std::vector<std::thread> threadArray(threadCount);
-  /*!< Pointer to barrier object*/
 
-  for(int i=0; i < threadArray.size(); i++){
-    threadArray[i]=std::thread(task,mutexSem,barrierSem,threadCount);
+  std::vector<std::thread> threadArray(threadCount);
+
+  for (std::vector<std::thread>::size_type i = 0; i < threadArray.size(); ++i) {
+    threadArray[i] = std::thread(task, mutexSem, barrierSem, threadCount);
   }
 
-  for(int i = 0; i < threadArray.size(); i++){
+   for (std::vector<std::thread>::size_type i = 0; i < threadArray.size(); ++i) {
     threadArray[i].join();
   }
-  
+
   return 0;
 }
