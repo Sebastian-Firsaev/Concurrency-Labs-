89f5a6b406479a79a3b6f1d57884518b53b9f334
Sebastian Firsaev <91489950+Sebastian-Firsaev@users.noreply.github.com>
2023-11-23 22:09:22 +0000

    Delete Lab 3 directory

--- Lab 3/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['semaphore_0',['Semaphore',['../class_semaphore.html',1,'']]],
-  ['setcount_1',['setCount',['../class_barrier.html#ab999e172844330d8b9a1c13f3766959d',1,'Barrier']]],
-  ['signal_2',['Signal',['../class_semaphore.html#a86f92f738b4486439b296d8e235895f2',1,'Semaphore']]]
-];
